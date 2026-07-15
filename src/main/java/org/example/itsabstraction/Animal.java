4b5218bbe388941c6d76df7f55480e1668641a42
jisvarun <jisbarunpandit@gmail.com>
2026-07-16 02:02:26 +0530

    its abstraction

---
+++ src/main/java/org/example/itsabstraction/Animal.java
@@ -0,0 +1,8 @@
+package org.example.itsabstraction;
+
+abstract class Animal {
+    public abstract void animalSound();
+    public void sleep(){
+        System.out.println("Zzz");
+    }
+}
---
+++ src/main/java/org/example/itsabstraction/Main.java
@@ -0,0 +1,9 @@
+package org.example.itsabstraction;
+
+public class Main {
+    public static void main(String[] args){
+        Pig myPig = new Pig();
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
