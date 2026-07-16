6decfde9010cccf510b33e62d457fe645925486e
jisvarun <jisbarunpandit@gmail.com>
2026-07-17 00:55:05 +0530

    Find the Greatest Common Divisor (GCD)

---
+++ src/main/java/org/example/FindGCD.java
@@ -0,0 +1,17 @@
+package org.example;
+// Find the Greatest Common Divisor (GCD)
+public class FindGCD {
+    public static void main(String[] args) {
+        int a = 36;
+        int b = 60;
+        int gcd = 1;
+
+        for(int i = 1; i <= a && i <= b; i++){
+            if(a % i == 0 && b % i == 0){
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD: " + gcd);
+    }
+}
