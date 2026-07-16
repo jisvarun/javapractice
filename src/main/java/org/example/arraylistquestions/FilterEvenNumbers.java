b53baeb70773500ac150ffeed7ea4e9a1ac982db
jisvarun <jisbarunpandit@gmail.com>
2026-07-17 01:24:00 +0530

    filter even numbers

--- src/main/java/org/example/arraylistquestions/FilterEvenNumbers.java
+++ src/main/java/org/example/arraylistquestions/FilterEvenNumbers.java
@@ -1,4 +1,29 @@
 package org.example.arraylistquestions;
 
+import java.util.ArrayList;
+
 public class FilterEvenNumbers {
+    public static void main(String[] args){
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+
+        System.out.println("Print number 1 to 10");
+        for(int i = 1; i<=10; i++){
+            numbers.add(i);
+        }
+
+        for(int num : numbers){
+            System.out.println(num);
+            if(num % 2 == 0){
+                evenNumbers.add(num);
+            }
+        }
+
+        System.out.println("Print even number: ");
+        for(int num : evenNumbers){
+            System.out.println(num);
+        }
+
+
+    }
 }
